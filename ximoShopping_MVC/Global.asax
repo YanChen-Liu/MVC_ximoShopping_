3c73140f070645ab064e73614dfafd9547eec221
sunuser <sunuser@DESKTOP-CUGS881>
2017-03-17 16:43:33 +0800

    加入專案檔案。

---
+++ ximoShopping_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ximoShopping_MVC.MvcApplication" Language="C#" %>
---
+++ ximoShopping_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ximoShopping_MVC.Startup))]
+namespace ximoShopping_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
