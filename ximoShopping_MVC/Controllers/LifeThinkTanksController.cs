8daff8cf9e5e074836e5f771e54b009fa0325ba5
sunuser <sunuser@DESKTOP-CUGS881>
2017-03-20 09:25:37 +0800

    加入資料庫連結

---
+++ ximoShopping_MVC/Controllers/LifeThinkTanksController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ximoShopping_MVC.Controllers
+{
+    public class LifeThinkTanksController : Controller
+    {
+        // GET: LifeThinkTanks
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
